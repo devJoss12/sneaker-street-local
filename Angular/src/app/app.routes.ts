0a14950f98a143baf4855b0ba90dddb18c0613b0
Dafne Reynoso <djrsdoge@gmail.com>
2024-10-29 11:12:47 -0600

    Interfaz login

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogoComponent } from './catalogo/catalogo.component';
 import { CarritoComponent } from './carrito/carrito.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },  // Página principal
+  { path: 'login', component: LoginComponent },
   { path: 'catalogo', component: CatalogoComponent },  // Página de catálogo
   { path: 'carrito', component: CarritoComponent },  // Página del carrito
 ];
