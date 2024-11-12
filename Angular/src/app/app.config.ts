73231dbfd1d46415c26fdb094d4daef5f898ab0a
devJoss12 <morenojos00@gmail.com>
2024-11-11 18:57:01 -0600

    Commit login 2

--- Angular/src/app/app.config.ts
+++ Angular/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
@@ -17,9 +16,8 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    importProvidersFrom(HttpClientModule),  // Asegúrate de importar HttpClientModule aquí
+    provideHttpClient(),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideAuth(() => getAuth())
-  
   ]
 };
